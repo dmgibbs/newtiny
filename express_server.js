449aedbbce6cc7591971380a8c18938b194eafb4
Damion Gibbs <dmgibbs@writeme.com>
2018-08-18 04:46:37 +0000

    added error page and modified UI.

--- express_server.js
+++ express_server.js
@@ -76,17 +76,23 @@ var urlDB = {
   "9te5xK": {longURL: "http://www.google.com",
              userid: "user2RandomID"},
   "3gDkE4": {longURL: "http://www.yahoo.com",
-             userid: "user4RandomID"}
+             userid: "user4RandomID"},
+  "2nAdkE": {longURL: "http://www.clarify.com",
+             userid: "abc182d"}
 };
 
 /*------------------------------------------------------
 * get the Id of who is logged in from the cookies variable
 --------------------------------------------------------*/
-function whoIsLoggedIn()
+function whoIsLoggedIn(shortU)
 {
-  let activeUser = req.cookies.user_id;
-  return activeUser
-};
+  for (let keys in urlDB){
+    if (keys  === shortU)
+      // console.log ("user is : -", user)   //.userid);
+      console.log("routine called with  of : ",urlDB[keys].userid);
+  }
+  return shortU;
+}
 
 /*------------------------------------------------------
 * function generates list of urls created by that user
@@ -122,14 +128,15 @@ function fetchIdFromDB(email) {
  /*---------------------------------------------------------------
  * Returns the userId stored, based on the email for that user.
  *-----------------------------------------------------------------*/ 
-   let theId = ""; 
-   for (var user in users)    {
-     if ( users[user]['email'] === email){   // if Emails match
-       theId = user;                         // grab the Id
-       break;
-     }
-   }
-   return theId;
+  let theId = ""; 
+  for (var user in users)    {
+    if (users[user]['email'] === email){   // if Emails match
+      theId = users[user].id;  
+      console.log("im grabbing",theId);                 // grab the Id
+      break;
+    }
+  }
+  return theId;
  }
 
 function isEmpty(str){
@@ -175,17 +182,12 @@ function fetchUser(id){
   }
 }
 
-function getShortUrls(theUser)
-{
-  
-
-
-}
-
 
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  //res.end("Hello!");
+  let errmsg = {err:"Page not Found"};
+  res.render("error",errmsg);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -195,11 +197,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   if (req.cookies && req.cookies.user_id) {
     var  userid = req.cookies.user_id;
+    console.log("This user has id of :",userid);
     let smallDB = urlsForUser(userid);
     let templateVars = {user: fetchUser(userid), urls:smallDB};
     res.render("urls_index", templateVars);      // Use template file urls_index.ejs located in views folder
-  }  else {
-    res.redirect("/login");
+  }else {
+    let errmsg = {err: "Cannot login due to an error with username/password."};
+    res.render("error",errmsg);
   }
 });
 
@@ -252,16 +256,21 @@ app.post("/login", (req, res) => {
   var userEmail = req.body.email;
   var userPass = req.body.password;
   if (foundEmail(users,userEmail) && foundPass(users,userPass)) {
-    res.cookie('user_id',fetchIdFromDB(userEmail));
+    console.log(" using this email :", userEmail);
+    console.log("He has id of :", fetchIdFromDB(userEmail) );
+    res.cookie("user_id",fetchIdFromDB(userEmail),{ maxAge: 10 * 60 * 1000 });
+    console.log('storing cookie of : ',res.cookie.user_id);
     res.redirect("/urls");
     return;
   } else if ( foundEmail(users,userEmail) && !foundPass(users,userPass)){
-    res.redirect("/login");
+    let errmsg = {err:"Unable to find the password. Please try again."};
+    res.render("error", errmsg);
     return;
-  }  else  {
-    res.redirect("/register");
-    return;
-  }
+  } 
+  //  else  {
+  //   res.redirect("/register");
+  //   return;
+  // }
 });
 
 app.post("/urls", (req, res) => {
@@ -282,7 +291,7 @@ app.post("/urls/:id", (req, res) => {
 app.post("/logout", (req, res) => {
   res.cookie("express.sid", "", { expires: new Date() });
   res.clearCookie('user_id');
-  res.redirect("/urls");
+  res.redirect("register");
 
 });
 
@@ -291,6 +300,13 @@ app.post("/register", (req, res) => {
   var userPass = req.body.password;
   var username = req.body.name;
  
+
+//   if (username && password && password_confirm) {
+//     if (password === password_confirm) {
+// bcrypt.hash(password, SALT_ROUNDS, (err, password_hashed) => {
+//   if (err) {
+//           console.log("There was an error hashing the password", err);
+//         } else {
   if (isEmpty(userEmail) || isEmpty(userPass)){
     res.status(401);
     res.redirect("/register");
@@ -303,18 +319,26 @@ app.post("/register", (req, res) => {
       res.redirect("/login");
       return;
   }
-    //Error checks passed ; register this user.
-    let uid = generateRandomString();  // get a random Id
-    res.cookie('user_id',uid);        // store this in cookie
-    users[uid]= {id: uid, email: userEmail, password: userPass}
-    res.redirect("/login");
-});
+
+  //   bcrypt.hash(password, SALT_ROUNDS, (err, password_hashed) => {
+  //   if (err) {
+  //     console.log("There was an error hashing the password", err);
+  // }  else { 
+       //Error checks passed ; register this user.
+          let uid = generateRandomString();  // get a random Id
+          res.cookie('user_id',uid);        // store this in cookie
+          users[uid]= {id: uid, email: userEmail, password: userPass}
+          res.redirect("/login");
+    //    }
+  //    });
+   });
 
 app.post("/urls/:id/delete", (req, res) => {
   let shortUrl = req.params.id;
-  //thisUser = whoisLoggedin();  // get id of current cookie user
-  // show shorturls of logged in person
-  //console.log("who is logged:", thisUser);
+  console.log("got to delete routine.")
+  thisUser = whoIsLoggedIn(req.params.id);  // get id of current cookie user
+  //// show shorturls of logged in person
+  console.log("the user called delete &  logged:", thisUser);
   //console.log("their shortUrls",getshortUrls(thisUser));
   delete urlDB[shortUrl];
   res.redirect("/urls");
